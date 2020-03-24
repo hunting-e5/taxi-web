5752cd1f0074fd87f4752a0393d34419950a3566
JeonSeungGyu <61815544+JeonSeungGyu@users.noreply.github.com>
2020-03-24 22:14:37 +0900

    v r0.1.0

---
+++ client/src/components/Tabs/LoginTab/pages/FindId.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FindID extends Component {
+    render() {
+        return (
+            <div>
+                아이디 찾기
+            </div>
+        );
+    }
+}
+
+export default FindID;
